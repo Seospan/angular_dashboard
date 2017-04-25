3eb8b0f0d32530e111c42ae7fa54a2dcf058091a
Seospan <sophie.jobez@gmail.com>
2017-04-25 10:49:04 +0200

    Local settings / prod settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,6 @@
 export class AppConfig {
-    public readonly apiUrl = 'http://37.59.31.134:8005';
-    public readonly apiRequestUrl = 'http://37.59.31.134:8005/api';
+    //public readonly apiUrl = 'http://37.59.31.134:8005';
+    //public readonly apiRequestUrl = 'http://37.59.31.134:8005/api';
+    public readonly apiUrl = 'http://localhost:8000';
+    public readonly apiRequestUrl = 'http://localhost:8000/api';
 };
